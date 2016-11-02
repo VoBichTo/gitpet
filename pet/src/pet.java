d6c42e164cdae0963b4c5b1733f53759b87a5bf6
Vo Bich To <vobichto1997@gmail.com>
2016-11-02 17:12:00 +0700

    Hello guy "Pet"

---
+++ pet/src/pet.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+
+public class pet extends JFrame {
+public pet(){
+	setTitle("Pet_97");
+	setSize(500,500);
+	setVisible(true);
+}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+pet pt = new pet();
+pt.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+	}
+
+}
