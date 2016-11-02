20cdd72195803c3434c7ecc96f2769e7ef7d8e41
Vo Bich To <vobichto1997@gmail.com>
2016-11-02 17:39:14 +0700

    ~~!

--- pet/src/pet.java
+++ pet/src/pet.java
@@ -1,21 +1,28 @@
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-
 public class pet extends JFrame {
-public pet(){
-	setTitle("Pet_97");
-	setSize(500,500);
-	setVisible(true);
-}
+	public pet() {
+		setTitle("Pet_97");
+		setVisible(true);
+		setSize(400,400);
+		setLayout(new FlowLayout());
+		for (int i = 0; i < 20; i++) {
+			add(new JButton(String.valueOf(i)));
+		}
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-pet pt = new pet();
-pt.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-
+		pet pt = new pet();
+		pt.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
 
 }
